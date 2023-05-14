60077b4409f075128451a97ee4266482e6f54bfa
Razza265 <127698055+Razza265@users.noreply.github.com>
2023-05-15 10:58:59 +1200

    Update Final version.py

--- Final version.py
+++ Final version.py
@@ -1,1 +1,34 @@
-print("welcome to the Maori quiz")
+import random
+
+# Define the quiz questions and answers as a dictionary
+quiz_questions = {
+    "What does 'one' mean? ": "tahi",
+    "What does 'two mean? ": "rua",
+    "What does 'three' mean? ": "toru",
+    "What does 'four' mean? ": "wha",
+    "What does 'five' mean? ": "rima"
+}
+
+# Shuffle the quiz questions
+quiz_order = list(quiz_questions.keys())
+random.shuffle(quiz_order)
+
+# Initialize a variable to keep track of the score
+score = 0
+
+# Loop through each question in the quiz
+for question in quiz_order:
+    # Ask the question and get the user's answer
+    answer = input(question)
+    # Check if the user's answer is correct
+    if answer.lower() == quiz_questions[question].lower():
+        # If the answer is correct, increase the score and display a message
+        score += 1
+        print("Correct!")
+    else:
+        # If the answer is incorrect, display the correct answer
+        print(f"Incorrect. The correct answer is {quiz_questions[question]}")
+
+# Display the final score
+print(f"Quiz complete! You got {score} out of {len(quiz_questions)} correct.")
+
